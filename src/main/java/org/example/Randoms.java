c00b9c7b5fc4dd45522cfe59cbf64e707ca5c84d
Freedixae <dubovoy671@gmail.com>
2020-10-06 17:17:08 +0600

    fixied

--- src/main/java/org/example/Randoms.java
+++ src/main/java/org/example/Randoms.java
@@ -29,4 +29,27 @@ public class Randoms implements Iterable<Integer> {
     public Iterator<Integer> iterator() {
         return new TodoIterator(this);
     }
+
+    public static class TodoIterator implements Iterator<Integer> {
+        protected Randoms target;
+
+        public TodoIterator(Randoms target) {
+            this.target = target;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public Integer next() {
+            int diff = target.getMax() - target.getMin();
+            return  target.getRandom().nextInt(diff + 1) + target.getMin();
+        }
+
+        @Override
+        public void remove() {
+        }
+    }
 }
